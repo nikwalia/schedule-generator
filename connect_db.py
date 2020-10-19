b2c9d5359256b293d64685701712007d53d01f27
nikwalia <nikash.walia@gmail.com>
2020-10-19 11:18:33 -0700

    Basic python script to access DB

---
+++ connect_db.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql://***REMOVED***')
+
+    with engine.connect() as connection:
+        result = connection.execute('SHOW tables FROM student_info')
+        for row in result:
+            print(row[0])
