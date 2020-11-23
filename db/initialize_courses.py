5d908e870580a6eb6054bd227fcb85d3d8001f8a
nikwalia <nikash.walia@gmail.com>
2020-11-23 09:27:09 -0800

    Add auto-tagging code and tags for CS-ENG tracks (#40)
    * Add auto-tagging code and tags for CS-ENG tracks
    
    * Add core requirements to other streams

--- db/initialize_courses.py
+++ db/initialize_courses.py
@@ -2,6 +2,8 @@ from utils import parse_data, extract_gpa_data, merge_gpa_data
 from mysql_engine import loadEngine, MySQLEngine
 import pathlib
 import numpy as np
+import json
+from tqdm import tqdm
 
 
 def upload_courses(file_path: str, engine: MySQLEngine):
@@ -21,10 +23,40 @@ def upload_courses(file_path: str, engine: MySQLEngine):
     gpa_df = extract_gpa_data(file_path)
     df = merge_gpa_data(df, gpa_df)
 
-    e.insert_df(df, 'courses')
+    engine.insert_df(df, 'courses')
+
+
+def tag_courses(file_path: str, engine: MySQLEngine):
+    """
+    Adds all tags to courses
+
+    :param file_path (string): where to find the JSON document with course info
+    :param engine (MySQLEngine): used to connect to RDMS
+    """
+    f = open(file_path, 'r')
+    course_tags = json.load(f)
+    f.close()
+
+    engine.raw_operation('SET SQL_SAFE_UPDATES = 0')
+    engine.raw_operation('UPDATE student_info.courses SET courses.interest = JSON_ARRAY()')
+    engine.raw_operation('SET SQL_SAFE_UPDATES = 1')
+
+    exec_command = "UPDATE student_info.courses " \
+                    "SET interest = JSON_ARRAY_APPEND(`interest`, '$', '{}') " \
+                    "WHERE course_id = '{}'"
+
+    for tag in course_tags:
+        tag_classes = course_tags[tag]
+        pbar = tqdm(total=len(tag_classes))
+        pbar.set_description("Processing {}".format(tag))
+        for tag_class in tag_classes:
+            engine.raw_operation(exec_command.format(tag, tag_class))
+            pbar.update(1)
+        del pbar
 
 
 if __name__ == '__main__':
     e = loadEngine()
     upload_courses('data', e)
+    tag_courses('db/static/track_data.json', e)
     del e
